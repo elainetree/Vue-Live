554d6a58a8e44d0d8ef9c6ab8a08e108ad329d2f
elaineliu7598 <girl7598@gmail.com>
2022-02-10 19:27:29 +0800

    新增：登入防呆

--- week2/js/login.js
+++ week2/js/login.js
@@ -14,24 +14,29 @@ const app = createApp({
   },
   methods:{
     login(){
-      // 要取到 data 中 API post 所需的參數，需使用 this
-      axios.post(`${apiUrl}/admin/signin`, this.user)
-      .then((res)=>{
-        // 在 res.data 中，可看到 token 和 expired，以解構的方式取出它們（expired 為 unix 時間戳格式）
-        const {token,expired} = res.data;
-
-        // 把 token 存到 cookie：定義存放 token 的名稱、到期日用 new Date 把時間戳轉成一般格式（取自 MDN - Document.cookie）
-        document.cookie = `elaineToken=${token}; expires=${new Date(expired)};`;
-
-        // 轉址到產品頁：window 是瀏覽器的物件，location 是 window 底下的轉址方法
-        window.location = 'products.html';
-      })
-      .catch((error)=>{
-        alert(error.data.message +'，請重新輸入');
-        // 登入失敗後清空欄位
-        this.user.username = '';
-        this.user.password = '';
-      })
+      // 防呆：帳密空值出現警告，有值才 call API
+      if(this.user.username === '' || this.user.password === ''){
+        alert('請輸入帳號和密碼');
+      }else{
+        // 要取到 data 中 API post 所需的參數，需使用 this
+        axios.post(`${apiUrl}/admin/signin`, this.user)
+        .then((res)=>{
+          // 在 res.data 中，可看到 token 和 expired，以解構的方式取出它們（expired 為 unix 時間戳格式）
+          const {token,expired} = res.data;
+  
+          // 把 token 存到 cookie：定義存放 token 的名稱、到期日用 new Date 把時間戳轉成一般格式（取自 MDN - Document.cookie）
+          document.cookie = `elaineToken=${token}; expires=${new Date(expired)};`;
+  
+          // 轉址到產品頁：window 是瀏覽器的物件，location 是 window 底下的轉址方法
+          window.location = 'products.html';
+        })
+        .catch((error)=>{
+          alert(error.data.message +'，請重新輸入');
+          // 登入失敗後清空欄位
+          this.user.username = '';
+          this.user.password = '';
+        })
+      }
     }
   }
 });
